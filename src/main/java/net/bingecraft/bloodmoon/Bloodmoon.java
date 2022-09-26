fe1014a3f49ef42cc19dadcfecf41fae45f7d528
tyler <tyler@cruftbusters.com>
2022-09-26 16:55:23 -0700

    add /bloodmoon command to set difficulty to hard

--- src/main/java/net/bingecraft/bloodmoon/Bloodmoon.java
+++ src/main/java/net/bingecraft/bloodmoon/Bloodmoon.java
@@ -1,17 +1,22 @@
 package net.bingecraft.bloodmoon;
 
+import org.bukkit.Difficulty;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Bloodmoon extends JavaPlugin {
-
+public final class Bloodmoon extends JavaPlugin implements CommandExecutor {
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getCommand("bloodmoon").setExecutor(this);
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        getServer().getWorld("world").setDifficulty(Difficulty.HARD);
+        sender.sendMessage("set difficulty to hard");
+        return true;
     }
+
 }
