97b210aa687255f32b42a97fe0d0986322db58ab
tyler <tyler@cruftbusters.com>
2022-09-27 11:33:50 -0700

    refactor - reformat java indent to 2/2/2

--- src/main/java/net/bingecraft/bloodmoon/Bloodmoon.java
+++ src/main/java/net/bingecraft/bloodmoon/Bloodmoon.java
@@ -4,14 +4,14 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Bloodmoon extends JavaPlugin implements CommandExecutor {
-    @Override
-    public void onEnable() {
-        Runnable task = new BloodmoonRepeatingTask(
-                new BloodmoonClock(72000L, 60000L, 73000L),
-                getServer().getWorld("world")
-        );
-        getServer()
-                .getScheduler()
-                .scheduleSyncRepeatingTask(this, task, 0L, 20L);
-    }
+  @Override
+  public void onEnable() {
+    Runnable task = new BloodmoonRepeatingTask(
+      new BloodmoonClock(72000L, 60000L, 73000L),
+      getServer().getWorld("world")
+    );
+    getServer()
+      .getScheduler()
+      .scheduleSyncRepeatingTask(this, task, 0L, 20L);
+  }
 }
--- src/main/java/net/bingecraft/bloodmoon/BloodmoonRepeatingTask.java
+++ src/main/java/net/bingecraft/bloodmoon/BloodmoonRepeatingTask.java
@@ -6,27 +6,27 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 public final class BloodmoonRepeatingTask implements Runnable {
-    private BloodmoonClock bloodmoonClock;
-    private World world;
+  private BloodmoonClock bloodmoonClock;
+  private World world;
 
-    BloodmoonRepeatingTask(BloodmoonClock bloodmoonClock, World world) {
-        this.bloodmoonClock = bloodmoonClock;
-        this.world = world;
+  BloodmoonRepeatingTask(BloodmoonClock bloodmoonClock, World world) {
+    this.bloodmoonClock = bloodmoonClock;
+    this.world = world;
+  }
+
+  @Override
+  public void run() {
+    Difficulty have = world.getDifficulty();
+    Difficulty want = bloodmoonClock.getDifficulty(world.getFullTime());
+    if (have != want) {
+      world.setDifficulty(want);
+      Bukkit.getConsoleSender().sendMessage("difficulty set to " + want);
     }
 
-    @Override
-    public void run() {
-        Difficulty have = world.getDifficulty();
-        Difficulty want = bloodmoonClock.getDifficulty(world.getFullTime());
-        if (have != want) {
-            world.setDifficulty(want);
-            Bukkit.getConsoleSender().sendMessage("difficulty set to " + want);
-        }
-
-        if (have != want && want == Difficulty.HARD) {
-            for (Player player : world.getPlayers()) {
-                player.sendMessage("the blood moon is rising");
-            }
-        }
+    if (have != want && want == Difficulty.HARD) {
+      for (Player player : world.getPlayers()) {
+        player.sendMessage("the blood moon is rising");
+      }
     }
+  }
 }
