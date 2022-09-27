b661f71750b659ea49d103750b0662a65eb778a3
tyler <tyler@cruftbusters.com>
2022-09-27 11:35:40 -0700

    add world name to difficulty change message

--- src/main/java/net/bingecraft/bloodmoon/BloodmoonRepeatingTask.java
+++ src/main/java/net/bingecraft/bloodmoon/BloodmoonRepeatingTask.java
@@ -5,6 +5,8 @@ import org.bukkit.Difficulty;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
+import java.text.MessageFormat;
+
 public final class BloodmoonRepeatingTask implements Runnable {
   private BloodmoonClock bloodmoonClock;
   private World world;
@@ -20,7 +22,8 @@ public final class BloodmoonRepeatingTask implements Runnable {
     Difficulty want = bloodmoonClock.getDifficulty(world.getFullTime());
     if (have != want) {
       world.setDifficulty(want);
-      Bukkit.getConsoleSender().sendMessage("difficulty set to " + want);
+      String message = MessageFormat.format("difficulty for {0} set to {1}", world.getName(), want);
+      Bukkit.getConsoleSender().sendMessage(message);
     }
 
     if (have != want && want == Difficulty.HARD) {
