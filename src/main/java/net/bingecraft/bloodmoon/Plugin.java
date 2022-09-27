3534d53b4480f93c251ef27857e894094392fd83
tyler <tyler@cruftbusters.com>
2022-09-27 12:16:25 -0700

    rename Bloodmoon to Plugin

--- src/main/java/net/bingecraft/bloodmoon/Plugin.java
+++ src/main/java/net/bingecraft/bloodmoon/Plugin.java
@@ -4,7 +4,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Bloodmoon extends JavaPlugin implements CommandExecutor {
+public final class Plugin extends JavaPlugin implements CommandExecutor {
   @Override
   public void onEnable() {
     FileConfiguration config = getConfig();
