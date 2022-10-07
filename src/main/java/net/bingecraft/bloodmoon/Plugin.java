807caa7d05dcac87bd33b0c4f132a1812e00d7c6
tyler <tyler@cruftbusters.com>
2022-10-07 16:45:40 -0700

    delete dead code

--- src/main/java/net/bingecraft/bloodmoon/Plugin.java
+++ src/main/java/net/bingecraft/bloodmoon/Plugin.java
@@ -5,7 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Plugin extends JavaPlugin implements CommandExecutor {
+public final class Plugin extends JavaPlugin {
   @Override
   public void onEnable() {
     FileConfiguration config = getConfig();
